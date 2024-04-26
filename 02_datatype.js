650b335e9d5db2b7248d409bd50924228d4a0815
Arjun807-art <147012064+Arjun807-art@users.noreply.github.com>
2024-04-26 12:20:52 +0530

    Add files via upload

---
+++ 02_datatype.js
@@ -0,0 +1,17 @@
+//  tc39 mdn plateform
+// primitive(boolen,Number,String,intiager,null,undefined,symbol(unique))
+// null datatype (object)
+// let score=null
+// console.log(score);
+
+// let valueInNumber =  100
+// console.log (typeof valueInNumber);
+
+// let isLoggedIn ="Arjun"
+// let booleanisLoggedIn = Boolean(isLoggedIn); 
+// console.log(booleanisLoggedIn);
+
+// let someNumber=33;
+// let StringNumber=String(someNumber)
+// console.log(StringNumber);
+// console.log( typeof (StringNumber));
