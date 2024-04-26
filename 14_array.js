dced9d81a347106e630281710364aa3997f11854
Arjun807-art <147012064+Arjun807-art@users.noreply.github.com>
2024-04-26 12:12:52 +0530

    Add files via upload

---
+++ 14_array.js
@@ -0,0 +1,49 @@
+// const myArr=["vivek","rani"]
+// console.log(myArr[0]);
+
+//  array method 
+// myArr.push(6) (add one array)
+// const vegetale=['onion','palak','carrot','gobi'];
+ 
+// console.log(vegetale.pop());
+// console.log(vegetale);
+// vegetale.pop();
+// console.log(vegetale); //(it is used to remove one arrat)
+// const arr =[1,2,4,6]
+//  console.log(arr.slice(1,-1));
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// fruits.copyWithin(2, 0, 2);
+// console.log(fruits)
+    
+//  const myArr=[1,2,4,5]
+//  console.log(myArr.shift());
+//  console.log(myArr)
+
+// const arr=[6,2,1,8,5,9]
+// console.log(arr.sort());
+
+// console.log(myArr.includes());
+// const newArr =myArr.join() (join conver array into strint)
+// console.log(myArr);
+// console.log (   newArr);
+
+// slice,splice
+// console.log("A", myArr);
+// const myn1= myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B",myArr);
+
+// const myn2=myArr.splice(1,3);
+// console.log("c",myArr);
+// console.log(myn2);
+
+// const myArr=[0,1,2,3,4,5,6,7,8];
+// const myArr2 =myArr.slice(1,5);
+// console.log(myArr);
+// console.log(myArr2);
+ 
+// const myArr=[0,1,2,3,4,5,6,];
+// const myArr2=myArr.splice(3,6);
+// console.log(myArr);
+// console.log(myArr2);
+
